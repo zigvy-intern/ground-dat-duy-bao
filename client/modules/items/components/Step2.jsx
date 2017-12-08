dcc4de395fb7eb09accfc9b4ccdb781809b470f6
Duy <nguyenthanhduy005@gmail.cm>
2017-12-08 08:41:08 +0700

    add Map Step2

--- client/modules/items/components/Step2.jsx
+++ client/modules/items/components/Step2.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import {Button, Modal, ButtonGroup, Glyphicon} from 'react-bootstrap';
+import {Button, Modal,Nav,Grid,Thumbnail, ButtonGroup,Navbar, Glyphicon,Container, Row, Col} from 'react-bootstrap';
+import GoogleMapReact from 'google-map-react';
+import Map from '../../items/components/Map.jsx';
 
-const Step2 = React.createClass({
+const Step1 = React.createClass({
   getInitialState() {
     return { show: false };
   },
@@ -11,8 +13,13 @@ const Step2 = React.createClass({
 
     return (
       <div className="modal-container" style={{ height: 200 }}>
-
-
+        <ButtonGroup>
+        <Button
+          bsStyle="primary"
+          onClick={() => this.setState({ show: true })}
+        >
+        BOOK NOW
+        </Button>
         <Modal
           show={this.state.show}
           onHide={close}
@@ -20,21 +27,50 @@ const Step2 = React.createClass({
           aria-labelledby="contained-modal-title"
         >
           <Modal.Header closeButton>
-            <Modal.Title id="contained-modal-title">Book your Match</Modal.Title>
+            <Modal.Title id="contained-modal-title">Completed</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            Elit est explicabo ipsum eaque dolorem blanditiis doloribus sed id ipsam, beatae, rem fuga id earum? Inventore et facilis obcaecati.
+          <div>
+            <img src="https://imgur.com/bNh6Djy.png" height="50" width="auto"/>
+            <br/>
+            <br/>
+            <p>Your Booking has been Issued!</p>
+            <Navbar>
+            <Grid>
+            <Row>
+            <Nav step2Left>
+            <Col xs={12} md={15}>
+              <Thumbnail style={{width:"150",height:"150"}}>
+              <Map/>
+              </Thumbnail>
+            </Col>
+            </Nav>
+
+            <Nav step2Right>
+            <Col xs={9} md={3}>
+              <p>Dia chi</p>
+            </Col>
+            </Nav>
+            </Row>
+            </Grid>
+            </Navbar>
+          </div>
           </Modal.Body>
           <Modal.Footer>
-            <Button onClick={close}>Close</Button>
+            <Button onClick={close}>Finish</Button>
           </Modal.Footer>
         </Modal>
-
-
+        <Button>
+        <Glyphicon glyph="star-empty"> Favorite</Glyphicon>
+        </Button>
+        <Button>
+        <Glyphicon glyph="share"> Share</Glyphicon>
+        </Button>
+        </ButtonGroup>
 
       </div>
     );
   },
 });
 
-export default Step2;
+export default Step1;
